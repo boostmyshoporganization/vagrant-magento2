526119804cc175317771de4492c6974e3f4925a5
Guillaume Dievart <dievartg@gmail.com>
2015-06-11 21:55:51 +0200

    Move the options of the install command on a unique line.

--- init.sh
+++ init.sh
@@ -59,22 +59,4 @@ server {
 /etc/init.d/nginx restart
 
 # Magento configuration
-php bin/magento setup:install \
-	--base-url=http://mage.dev \
-	--backend-frontname=admin \
-	--db-host=localhost \
-	--db-name=magento2 \
-	--db-user=root \
-	--db-password=magento2 \
-	--admin-firstname=Magento \
-	--admin-lastname=User \
-	--admin-email=user@example.com \
-	--admin-user=admin \
-	--admin-password=admin123 \
-	--language=en_US \
-	--currency=USD \
-	--timezone=America/Chicago \
-	--cleanup-database \
-	--sales-order-increment-prefix="ORD$" \
-	--session-save=db \
-	--use-rewrites=1
+php bin/magento setup:install --base-url=http://mage.dev --backend-frontname=admin --db-name=magento2 --db-user=root --db-password=magento2 --admin-firstname=Magento --admin-lastname=User --admin-email=user@example.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1
