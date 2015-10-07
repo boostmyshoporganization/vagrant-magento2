b3565b2f5bb5e264ea1265b8f5e2f95fdff14601
Guillaume Dievart <dievartg@gmail.com>
2015-10-07 21:27:05 +0200

    Change the directory owner

--- init.sh
+++ init.sh
@@ -60,3 +60,6 @@ server {
 
 # Magento configuration
 php bin/magento setup:install --base-url=http://mage.dev --backend-frontname=admin --db-name=magento2 --db-user=root --db-password=magento2 --admin-firstname=Magento --admin-lastname=User --admin-email=user@example.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1
+
+chmod -R 777 var/
+chown -R vagrant:vagrant .
